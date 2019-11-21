0e8ddf0c78f6b25fd9c725592b58f6f7036e8af6
nicolas vit <nicovitca@gmail.com>
2019-11-21 11:50:40 +0000

    typed.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,6 @@ import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 initMapbox();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
